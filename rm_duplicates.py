09cd547f91ee3458b069c60dc424ab294cb1c0d4
chouzee <anton.ryabchenko.92@gmail.com>
2020-11-24 13:27:10 +0200

    Add another solution from solution section

--- rm_duplicates.py
+++ rm_duplicates.py
@@ -4,3 +4,16 @@ def rm_duplicates(user_list):
 user_list = ['one', 'one', 12, 15, 12]
 
 print(rm_duplicates(user_list))
+
+
+#another approach through loop iter
+# from the solution section
+
+dup_items = set()
+uniq_items = []
+for a in user_list:
+    if a not in dup_items:
+        uniq_items.append(a)
+        dup_items.add(a)
+        
+print(dup_items)
