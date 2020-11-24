0c2e363428372b5e119f26ac7e37c1946842819b
chouzee <anton.ryabchenko.92@gmail.com>
2020-11-24 08:59:15 +0200

    Added a solution from w3resource as an example

--- swap_chars.py
+++ swap_chars.py
@@ -5,3 +5,14 @@ def repl_two_strings(first, last):
 
 
 print(repl_two_strings("abc", "xyz"))
+
+
+# here is a solution from w3source
+# just put it here, looks much logical than mine
+
+def chars_mix_up(a, b):  
+  new_a = b[:2] + a[2:]  
+  new_b = a[:2] + b[2:]  
+  
+  return new_a + ' ' + new_b  
+print(chars_mix_up('abc', 'xyz'))  
